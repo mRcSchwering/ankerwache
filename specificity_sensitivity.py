28545ff90a32e4d189d4cf1ec2f421213cef93db
marc <schweringmarc01@gmail.com>
2021-11-18 15:53:35 +0100

    simulated drifting away

--- specificity_sensitivity.py
+++ specificity_sensitivity.py
@@ -3,9 +3,82 @@ Here, I am trying out some settings for the anchor watch.
 I am estimating the specificity (don't wake me up if the anchor is not really dragging)
 and the sensitivity (if the anchor is really dragging, wake me up).
 Naturally, while one goes up, the other tends to go down.
+
+    python specificity_sensitivity.py
+
+100 trials, 5,760 requests (that's 1 request every 5s for 8h)
+
+
+Counter: counter1
+
+Raising false alarms
+Count to 3: Alarm raised 49.00% of the time
+Count to 4: Alarm raised 6.00% of the time
+Count to 5: Alarm raised 0.00% of the time
+
+Raising True Alarms at the 2SD border
+Count to 3: Alarm raised 100.00% of the time
+Count to 4: Alarm raised 100.00% of the time
+Count to 5: Alarm raised 100.00% of the time
+Count to 3: Alarm raised after 35.0+/-15.0s
+Count to 4: Alarm raised after 67.5+/-37.5s
+Count to 5: Alarm raised after 105.0+/-50.0s
+
+Raising True Alarms at the 3SD border
+Count to 3: Alarm raised 100.00% of the time
+Count to 4: Alarm raised 100.00% of the time
+Count to 5: Alarm raised 100.00% of the time
+Count to 3: Alarm raised after 15.0+/-0.0s
+Count to 4: Alarm raised after 20.0+/-0.0s
+Count to 5: Alarm raised after 30.0+/-5.0s
+
+Drift until alarm with rate of 0.01SD
+Count to 3: drifted 1.58+/-0.18SD, that's 32+/-4m with SD=20m
+Count to 4: drifted 1.74+/-0.20SD, that's 35+/-4m with SD=20m
+Count to 5: drifted 1.98+/-0.13SD, that's 40+/-3m with SD=20m
+
+Drift until alarm with rate of 0.001SD
+Count to 3: drifted 0.99+/-0.16SD, that's 20+/-3m with SD=20m
+Count to 4: drifted 1.29+/-0.12SD, that's 26+/-2m with SD=20m
+Count to 5: drifted 1.50+/-0.13SD, that's 30+/-3m with SD=20m
+
+
+Counter: counter2
+
+Raising false alarms
+Count to 3: Alarm raised 50.00% of the time
+Count to 4: Alarm raised 5.00% of the time
+Count to 5: Alarm raised 0.00% of the time
+
+Raising True Alarms at the 2SD border
+Count to 3: Alarm raised 100.00% of the time
+Count to 4: Alarm raised 100.00% of the time
+Count to 5: Alarm raised 100.00% of the time
+Count to 3: Alarm raised after 45.0+/-20.0s
+Count to 4: Alarm raised after 57.5+/-27.5s
+Count to 5: Alarm raised after 115.0+/-45.0s
+
+Raising True Alarms at the 3SD border
+Count to 3: Alarm raised 100.00% of the time
+Count to 4: Alarm raised 100.00% of the time
+Count to 5: Alarm raised 100.00% of the time
+Count to 3: Alarm raised after 15.0+/-0.0s
+Count to 4: Alarm raised after 25.0+/-5.0s
+Count to 5: Alarm raised after 30.0+/-5.0s
+
+Drift until alarm with rate of 0.01SD
+Count to 3: drifted 1.52+/-0.24SD, that's 30+/-5m with SD=20m
+Count to 4: drifted 1.73+/-0.22SD, that's 35+/-4m with SD=20m
+Count to 5: drifted 2.00+/-0.14SD, that's 40+/-3m with SD=20m
+
+Drift until alarm with rate of 0.001SD
+Count to 3: drifted 0.96+/-0.20SD, that's 19+/-4m with SD=20m
+Count to 4: drifted 1.29+/-0.13SD, that's 26+/-3m with SD=20m
+Count to 5: drifted 1.51+/-0.11SD, that's 30+/-2m with SD=20m
 """
-from scipy.stats import bernoulli, norm, median_absolute_deviation  # tpye:ignore
-from numpy import median, mean
+from typing import Callable
+from scipy.stats import bernoulli, norm, median_abs_deviation  # type:ignore
+from numpy import median  # type:ignore
 
 
 def alarmCounter1(nums: list, count: int) -> int:
@@ -34,79 +107,72 @@ def alarmCounter2(nums: list, count: int) -> int:
     return -1
 
 
-def raise_at_least_1_alarm1(N: int, n: int, p: float, c: int) -> float:
-    alarms = [alarmCounter1(nums=bernoulli.rvs(p=p, size=n), count=c) for _ in range(N)]
-    return sum([d > 0 for d in alarms]) / N
+def raise_at_least_1_alarm(N: int, n: int, p: float, c: int, counter: Callable):
+    alarms = [counter(nums=bernoulli.rvs(p=p, size=n), count=c) for _ in range(N)]
+    print(
+        f"Count to {c}: Alarm raised {sum([d > 0 for d in alarms]) / N * 100:.2f}% of the time"
+    )
 
 
-def when_was_alarm_raised1(N: int, n: int, p: float, c: int) -> tuple:
-    alarms = [alarmCounter1(nums=bernoulli.rvs(p=p, size=n), count=c) for _ in range(N)]
+def when_was_alarm_raised(N: int, n: int, p: float, c: int, counter: Callable):
+    alarms = [counter(nums=bernoulli.rvs(p=p, size=n), count=c) for _ in range(N)]
     raised = [d for d in alarms if d > 0]
-    return (median(raised), median_absolute_deviation(raised))
+    med_s = (median(raised) + 1) * 5
+    mad_s = median_abs_deviation(raised) * 5
+    print(f"Count to {c}: Alarm raised after {med_s:.1f}+/-{mad_s:.1f}s")
 
 
-def raise_at_least_1_alarm2(N: int, n: int, p: float, c: int) -> float:
-    alarms = [alarmCounter2(nums=bernoulli.rvs(p=p, size=n), count=c) for _ in range(N)]
-    return sum([d > 0 for d in alarms]) / N
-
-
-def when_was_alarm_raised2(N: int, n: int, p: float, c: int) -> tuple:
-    alarms = [alarmCounter2(nums=bernoulli.rvs(p=p, size=n), count=c) for _ in range(N)]
+def drifting_away(rate: float, c: int, counter: Callable, N: int):
+    sd = 20
+    steps = [i * rate for i in range(4 * int(1 / rate))]
+    ps = [1 - (norm.cdf(2 - d) - norm.cdf(-2 - d)) for d in steps]
+    alarms = []
+    for _ in range(N):
+        nums = [bernoulli.rvs(p=p, size=1) for p in ps]
+        alarms.append(counter(nums=nums, count=c))
     raised = [d for d in alarms if d > 0]
-    return (median(raised), median_absolute_deviation(raised))
+    med_rate = (median(raised) + 1) * rate
+    mad_rate = median_abs_deviation(raised) * rate
+    print(
+        f"Count to {c}: drifted {med_rate:.2f}+/-{mad_rate:.2f}SD, that's {med_rate * sd:.0f}+/-{mad_rate * sd:.0f}m with SD=20m"
+    )
 
 
-# 10k trials, watch is active 8h
-N = 10_000
-n = int(8 * 60 * 60 / 5)
+if __name__ == "__main__":
+    N = 100
+    n = int(8 * 60 * 60 / 5)
+    print(f"\n\n{N} trials, {n:,} requests (that's 1 request every 5s for 8h)")
 
-# Counter 1
+    for name, counter in [("counter1", alarmCounter1), ("counter2", alarmCounter2)]:
+        print(f"\n\nCounter: {name}")
 
-# False Alarm cases
-raise_at_least_1_alarm1(N=N, n=n, p=0.05, c=3)  # 51.0%
-raise_at_least_1_alarm1(N=N, n=n, p=0.05, c=4)  # 3.8%
-raise_at_least_1_alarm1(N=N, n=n, p=0.05, c=5)  # 0.2%
+        print("\nRaising false alarms")
+        raise_at_least_1_alarm(N=N, n=n, p=0.05, c=3, counter=alarmCounter1)
+        raise_at_least_1_alarm(N=N, n=n, p=0.05, c=4, counter=alarmCounter1)
+        raise_at_least_1_alarm(N=N, n=n, p=0.05, c=5, counter=alarmCounter1)
 
-# True Alarm cases (just at 2 SD border)
-mean(norm.rvs(loc=2, size=10000) <= 2)  # 50% chance to be outside
-raise_at_least_1_alarm1(N=N, n=n, p=0.5, c=3)  # 100.0%
-raise_at_least_1_alarm1(N=N, n=n, p=0.5, c=4)  # 100.0%
-raise_at_least_1_alarm1(N=N, n=n, p=0.5, c=5)  # 100.0%
-when_was_alarm_raised1(N=N, n=n, p=0.5, c=3)  # 45s +/- 35s
-when_was_alarm_raised1(N=N, n=n, p=0.5, c=4)  # 70s +/- 60s
-when_was_alarm_raised1(N=N, n=n, p=0.5, c=5)  # 110s +/- 80s
+        print(f"\nRaising True Alarms at the 2SD border")
+        raise_at_least_1_alarm(N=N, n=n, p=0.5, c=3, counter=alarmCounter1)
+        raise_at_least_1_alarm(N=N, n=n, p=0.5, c=4, counter=alarmCounter1)
+        raise_at_least_1_alarm(N=N, n=n, p=0.5, c=5, counter=alarmCounter1)
+        when_was_alarm_raised(N=N, n=n, p=0.5, c=3, counter=alarmCounter1)
+        when_was_alarm_raised(N=N, n=n, p=0.5, c=4, counter=alarmCounter1)
+        when_was_alarm_raised(N=N, n=n, p=0.5, c=5, counter=alarmCounter1)
 
-# True Alarm cases (just at 3 SD border)
-mean(norm.rvs(loc=3, size=1000) <= 2)  # 85% chance to be outside
-raise_at_least_1_alarm1(N=N, n=n, p=0.85, c=3)  # 100.0%
-raise_at_least_1_alarm1(N=N, n=n, p=0.85, c=4)  # 100.0%
-raise_at_least_1_alarm1(N=N, n=n, p=0.85, c=5)  # 100.0%
-when_was_alarm_raised1(N=N, n=n, p=0.85, c=3)  # 15s +/- 0s
-when_was_alarm_raised1(N=N, n=n, p=0.85, c=4)  # 20s +/- 0s
-when_was_alarm_raised1(N=N, n=n, p=0.85, c=5)  # 35s +/- 5s
+        print(f"\nRaising True Alarms at the 3SD border")
+        raise_at_least_1_alarm(N=N, n=n, p=0.85, c=3, counter=alarmCounter1)
+        raise_at_least_1_alarm(N=N, n=n, p=0.85, c=4, counter=alarmCounter1)
+        raise_at_least_1_alarm(N=N, n=n, p=0.85, c=5, counter=alarmCounter1)
+        when_was_alarm_raised(N=N, n=n, p=0.85, c=3, counter=alarmCounter1)
+        when_was_alarm_raised(N=N, n=n, p=0.85, c=4, counter=alarmCounter1)
+        when_was_alarm_raised(N=N, n=n, p=0.85, c=5, counter=alarmCounter1)
 
+        print(f"\nDrift until alarm with rate of 0.01SD")
+        drifting_away(rate=0.01, c=3, counter=alarmCounter1, N=N)
+        drifting_away(rate=0.01, c=4, counter=alarmCounter1, N=N)
+        drifting_away(rate=0.01, c=5, counter=alarmCounter1, N=N)
 
-# Counter 2
-
-# False Alarm cases
-raise_at_least_1_alarm2(N=N, n=n, p=0.05, c=3)  # 50.0%
-raise_at_least_1_alarm2(N=N, n=n, p=0.05, c=4)  # 3.3%
-raise_at_least_1_alarm2(N=N, n=n, p=0.05, c=5)  # 0.1%
-
-# True Alarm cases (just at 2 SD border)
-mean(norm.rvs(loc=2, size=1000) <= 2)  # 50% chance to be outside
-raise_at_least_1_alarm2(N=N, n=n, p=0.5, c=3)  # 100.0%
-raise_at_least_1_alarm2(N=N, n=n, p=0.5, c=4)  # 100.0%
-raise_at_least_1_alarm2(N=N, n=n, p=0.5, c=5)  # 100.0%
-when_was_alarm_raised2(N=N, n=n, p=0.5, c=3)  # 50s +/- 45s
-when_was_alarm_raised2(N=N, n=n, p=0.5, c=4)  # 110s +/- 100s
-when_was_alarm_raised2(N=N, n=n, p=0.5, c=5)  # 220s +/- 210s
-
-# True Alarm cases (just at 3 SD border)
-mean(norm.rvs(loc=3, size=1000) <= 2)  # 85% chance to be outside
-raise_at_least_1_alarm2(N=N, n=n, p=0.85, c=3)  # 100.0%
-raise_at_least_1_alarm2(N=N, n=n, p=0.85, c=4)  # 100.0%
-raise_at_least_1_alarm2(N=N, n=n, p=0.85, c=5)  # 100.0%
-when_was_alarm_raised2(N=N, n=n, p=0.85, c=3)  # 15s +/- 0s
-when_was_alarm_raised2(N=N, n=n, p=0.85, c=4)  # 20s +/- 0s
-when_was_alarm_raised2(N=N, n=n, p=0.85, c=5)  # 30s +/- 5s
+        print(f"\nDrift until alarm with rate of 0.001SD")
+        drifting_away(rate=0.001, c=3, counter=alarmCounter1, N=N)
+        drifting_away(rate=0.001, c=4, counter=alarmCounter1, N=N)
+        drifting_away(rate=0.001, c=5, counter=alarmCounter1, N=N)
